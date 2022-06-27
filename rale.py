d2f0da4ce45b54b6c238f9a84c135f124e1b4998
gouhen <gwen.berson@gmail.com>
2022-06-27 15:17:01 +0200

    class in specific files

--- rale.py
+++ rale.py
@@ -9,14 +9,3 @@ class Rale:
   def shout(self):
     return random.choice(self.shoutsOfRage)
 
-class Content:
-  happyThoughts = [ ":smiling_face_with_3_hearts: Le soleil brille, les oiseaux chantent!"
-                  , "Youpi, youpi!! :zany_face:"
-                  , "Yipikaïïï!! "
-                  , "Le soleil chante, les oiseaux brillent! :zany_face:"
-                  , "Chuis hyper conteeeent(e) :face_vomiting: !!"
-                  ]
-
-  def happy(self):
-    return random.choice(self.happyThoughts)
-
